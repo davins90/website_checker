be7fe0bd668510d9c755c24712a1f2045d28f695
dani <daniele.davino@live.it>
2023-07-02 02:07:49 +0200

    edit main

--- main.py
+++ main.py
@@ -36,8 +36,7 @@ def check_website(request):
     headers = {
         "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Mobile Safari/537.36"
     }
-    return headers  
-#     response = requests.get(url,headers)
+    return requests.get(url,headers)
 #     status_code = response.status_code
 #     current_hash = hashlib.sha224(response.text.encode('utf-8')).hexdigest()
     
